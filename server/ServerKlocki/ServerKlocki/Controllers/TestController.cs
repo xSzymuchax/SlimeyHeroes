7f1439330f39a5a883550e13e2b56057e86ccf23
Szymon Pietrzak <szymonpietrzak55@gmail.com>
2025-08-07 17:32:57 +0200

    test controller, testing syntax

---
+++ server/ServerKlocki/ServerKlocki/Controllers/TestController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+namespace ServerKlocki.Controllers
+{
+    [ApiController]
+    [Route("/test")]
+    public class TestController : ControllerBase
+    {
+        public class NumberDTO
+        {
+            public int Number { get; set; }
+        }
+
+        [HttpGet]
+        public IActionResult TestHello()
+        {
+            var response = new[] {
+                new { message = "Hello from test" },
+                new { message = "Another hello" }
+            };
+
+            return Ok(response);
+        }
+
+        [HttpGet("{param}")]
+        public IActionResult TestParamSquared(int param)
+        {
+            return Ok(param * param);
+        }
+
+        [HttpPost]
+        public IActionResult TestPost([FromBody] NumberDTO number)
+        {
+            return Ok(number.Number * 2);
+        }
+    }
+}
--- server/ServerKlocki/ServerKlocki/Program.cs
+++ server/ServerKlocki/ServerKlocki/Program.cs
@@ -5,10 +5,14 @@ namespace ServerKlocki
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllers();
+
             var app = builder.Build();
 
+            app.MapControllers();
             app.MapGet("/", () => "Hello World!");
 
+            //app.UseHttpsRedirection();
             app.Run();
         }
     }
