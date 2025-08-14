0d1b63c9b548170f04a91c4315e392172ab3b716
Szymon Pietrzak <szymonpietrzak55@gmail.com>
2025-08-14 16:15:15 +0200

    files restructure

--- client/Assets/Scripts/Gameboard/Elements/LightningElement.cs
+++ client/Assets/Scripts/Gameboard/Elements/LightningElement.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class YellowElement : Element
+public class LightningElement : Element
 {
 
 }
--- client/Assets/Scripts/Gameboard/Elements/WaterElement.cs
+++ client/Assets/Scripts/Gameboard/Elements/WaterElement.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class OrangeElement : Element
+public class WaterElement : Element
 {
 
 }
